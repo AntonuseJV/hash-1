3341cff8ae7909bbf882bccb5837b8ceb7771026
AntonuseJV <anton954@rambler.ru>
2022-12-01 17:20:41 +0300

    Added demonstration program in Main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-
     static String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
             "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
             "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
